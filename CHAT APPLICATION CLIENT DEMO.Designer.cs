1baa660313c572cee01fd0583cb0eb3029138ec1
BungPeerapat <mashiro74122541@gmail.com>
2020-04-08 15:48:55 +0700

    Save

--- CHAT APPLICATION CLIENT DEMO.Designer.cs
+++ CHAT APPLICATION CLIENT DEMO.Designer.cs
@@ -31,14 +31,19 @@
             this.TxtMessageDemo = new System.Windows.Forms.TextBox();
             this.IPDEMO = new System.Windows.Forms.TextBox();
             this.PORTDEMO = new System.Windows.Forms.TextBox();
+            this.ConnectBotton = new System.Windows.Forms.Button();
+            this.SendTextDemo = new System.Windows.Forms.TextBox();
+            this.SendBottonDemo = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // TxtMessageDemo
             // 
-            this.TxtMessageDemo.Location = new System.Drawing.Point(5, 104);
+            this.TxtMessageDemo.Location = new System.Drawing.Point(5, 73);
             this.TxtMessageDemo.Multiline = true;
             this.TxtMessageDemo.Name = "TxtMessageDemo";
-            this.TxtMessageDemo.Size = new System.Drawing.Size(1137, 519);
+            this.TxtMessageDemo.Size = new System.Drawing.Size(1122, 480);
             this.TxtMessageDemo.TabIndex = 0;
             // 
             // IPDEMO
@@ -57,11 +62,43 @@
             this.PORTDEMO.TabIndex = 2;
             this.PORTDEMO.Text = "1443";
             // 
+            // ConnectBotton
+            // 
+            this.ConnectBotton.Location = new System.Drawing.Point(983, 25);
+            this.ConnectBotton.Name = "ConnectBotton";
+            this.ConnectBotton.Size = new System.Drawing.Size(121, 41);
+            this.ConnectBotton.TabIndex = 3;
+            this.ConnectBotton.Text = "Connect";
+            this.ConnectBotton.UseVisualStyleBackColor = true;
+            this.ConnectBotton.Click += new System.EventHandler(this.ConnectBotton_Click);
+            // 
+            // SendTextDemo
+            // 
+            this.SendTextDemo.Location = new System.Drawing.Point(4, 561);
+            this.SendTextDemo.Multiline = true;
+            this.SendTextDemo.Name = "SendTextDemo";
+            this.SendTextDemo.Size = new System.Drawing.Size(941, 49);
+            this.SendTextDemo.TabIndex = 4;
+            // 
+            // SendBottonDemo
+            // 
+            this.SendBottonDemo.Location = new System.Drawing.Point(957, 568);
+            this.SendBottonDemo.Name = "SendBottonDemo";
+            this.SendBottonDemo.Size = new System.Drawing.Size(169, 41);
+            this.SendBottonDemo.TabIndex = 5;
+            this.SendBottonDemo.Text = "SEND";
+            this.SendBottonDemo.UseVisualStyleBackColor = true;
+            this.SendBottonDemo.Click += new System.EventHandler(this.SendBottonDemo_Click);
+            // 
             // CHAT_APPLICATION_CLIENT_DEMO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1139, 622);
+            this.Controls.Add(this.SendBottonDemo);
+            this.Controls.Add(this.SendTextDemo);
+            this.Controls.Add(this.ConnectBotton);
             this.Controls.Add(this.PORTDEMO);
             this.Controls.Add(this.IPDEMO);
             this.Controls.Add(this.TxtMessageDemo);
@@ -77,5 +114,10 @@
         private System.Windows.Forms.TextBox TxtMessageDemo;
         private System.Windows.Forms.TextBox IPDEMO;
         private System.Windows.Forms.TextBox PORTDEMO;
+        private System.Windows.Forms.Button ConnectBotton;
+        private System.Windows.Forms.TextBox SendTextDemo;
+        private System.Windows.Forms.Button SendBottonDemo;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
     }
 }
--- CHAT APPLICATION CLIENT DEMO.cs
+++ CHAT APPLICATION CLIENT DEMO.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceModel.Activities;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,50 @@ namespace SYSTEMDEMO
 {
     public partial class CHAT_APPLICATION_CLIENT_DEMO : Form
     {
+        private TcpClient client;
+        public StreamReader SR;
+        public StreamWriter SW;
+        public string receive;
+        public string sendtxtclient;
         public CHAT_APPLICATION_CLIENT_DEMO()
         {
             InitializeComponent();
+        }
+
+        private void ConnectBotton_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint ip_end = new IPEndPoint(IPAddress.Parse(IPDEMO.Text), int.Parse(PORTDEMO.Text));
+            try
+            {
+                if (client.Connected)
+                {
+                    MessageBox.Show("Connected!");
+                    Console.Beep();
+                    SW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SendBottonDemo_Click(object sender, EventArgs e)
+        {
+            if (client.Connected)
+            {
+                if (!string.IsNullOrEmpty(SendTextDemo.Text))
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show
+            }
         }
     }
 }
